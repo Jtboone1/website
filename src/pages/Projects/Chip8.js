d693e3760059caae3666643b6693f633350af6e4
Jtboone1 <boojarrod@gmail.com>
2021-07-06 09:51:56 -0230

    Added wasm configurations

--- src/pages/Projects/Chip8.js
+++ src/pages/Projects/Chip8.js
@@ -1,9 +1,18 @@
 import * as React from 'react';
+import { CHIP8 } from 'boone8';
+import { memory } from 'boone8/boone8_bg.wasm';
 
 import * as css from '../../css/chip8Page.module.css';
 import '../../css/global.module.css';
 
+const chip = CHIP8.new();
+const memPtr = chip.get_memory();
+const chipMem = new Uint8Array(memory.buffer, memPtr, 4096)
+console.log(chipMem[0x050]);
+
+
 const Chip8 = () => {
+
     return (
         <>
             <main className={css.cssMain}>
